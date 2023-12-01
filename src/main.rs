161a9cb8cbf35362838f9900c51b768e02905516
taigi100 <contact@taigi100.com>
2023-12-01 11:22:07 +0200

    day 1

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+use std::fs;
+
+fn day1() {
+    let data = fs::read_to_string("data/day1.in").unwrap();
+    let lines =data.lines();
+    let mut sum = 0;
+    lines.for_each(|line| sum += line.chars().find(|&c| c.is_ascii_digit()).unwrap().to_digit(10).unwrap() * 10 + line.chars().rev().find(|&c| c.is_ascii_digit()).unwrap().to_digit(10).unwrap());
+    println!("{}", sum);
+}
 fn main() {
-    println!("Hello, world!");
+   day1();
 }
