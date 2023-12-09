d45a1e016f70bf0f9ef1a9b448d838c5cbac0c0c
taigi100 <contact@taigi100.com>
2023-12-09 08:10:55 +0200

    add rustfmt

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,34 @@
-use std::{fs, io};
+use itertools::Itertools;
+use num::integer;
 use std::cmp::{max, Ordering};
 use std::collections::HashMap;
 use std::time::Instant;
-use itertools::Itertools;
-use num::integer;
+use std::{fs, io};
 
 fn day9() -> io::Result<(i32, i32)> {
     let data = fs::read_to_string("data/day9.in").unwrap();
-    let (mut p1, mut p2) = (0, 0);
-    let values = data.lines().map(|line| line.split(' ').map(|x| x.parse::<i32>().unwrap()).collect::<Vec<_>>()).collect::<Vec<_>>();
-
+    let values = data
+        .lines()
+        .map(|line| {
+            line.split(' ')
+                .map(|x| x.parse::<i32>().unwrap())
+                .collect::<Vec<_>>()
+        })
+        .collect::<Vec<_>>();
     fn triangles(mut nums: Vec<i32>) -> (i32, i32) {
         if nums.iter().all(|&x| x == 0) {
-            nums.push(0);
-            nums.push(0);
+            nums.extend([0, 0]);
             return (0, 0);
         }
-        let mut new_nums = Vec::new();
-        for i in 0..nums.len() - 1 {
-           new_nums.push(nums[i+1] - nums[i]);
-        }
-        let (left, right) = triangles(new_nums);
+        let (left, right) = triangles(nums.windows(2).map(|w| w[1] - w[0]).collect_vec());
         nums.push(nums[nums.len() - 1] + right);
         nums.insert(0, nums[0] - left);
-        return (nums[0], nums[nums.len()-1]);
+        (nums[0], nums[nums.len() - 1])
     }
-    for value in &values {
-        p1 += triangles(value.clone()).1;
-        p2 += triangles(value.clone()).0;
-    }
-    println!("{:?}", values);
+    let (p2, p1) = values
+        .iter()
+        .map(|v| triangles(v.clone()))
+        .fold((0, 0), |(s1, s2), (t1, t2)| (s1 + t1, s2 + t2));
     Ok((p1, p2))
 }
 fn day8() -> io::Result<(usize, usize)> {
@@ -38,14 +37,23 @@ fn day8() -> io::Result<(usize, usize)> {
     let mut graph: HashMap<String, (String, String)> = HashMap::new();
     let moves = data.lines().next().unwrap();
     for line in data.lines().skip(2) {
-        let (origin, left, right) = line.trim_end().split([' ', '=', '(', ',', ')']).filter(|s| !s.is_empty()).collect_tuple().unwrap();
+        let (origin, left, right) = line
+            .trim_end()
+            .split([' ', '=', '(', ',', ')'])
+            .filter(|s| !s.is_empty())
+            .collect_tuple()
+            .unwrap();
         graph.insert(origin.to_string(), (left.to_string(), right.to_string()));
     }
     let mut current = "AAA";
     if graph.contains_key(current) {
         while current != "ZZZ" {
             let dir = moves.chars().nth(p1 % moves.len()).unwrap();
-            current = if dir == 'L' { graph[current].0.as_str() } else { graph[current].1.as_str() };
+            current = if dir == 'L' {
+                graph[current].0.as_str()
+            } else {
+                graph[current].1.as_str()
+            };
             p1 += 1;
         }
     }
@@ -57,32 +65,49 @@ fn day8() -> io::Result<(usize, usize)> {
         let mut num_moves = 0;
         while !node.ends_with("Z") {
             let dir = moves.chars().nth(num_moves % moves.len()).unwrap();
-            node = if dir == 'L' { &graph[node].0 } else { &graph[node].1 };
+            node = if dir == 'L' {
+                &graph[node].0
+            } else {
+                &graph[node].1
+            };
             num_moves += 1;
         }
         durations.push(num_moves);
     }
 
-    p2 = durations.iter().cloned().fold(1, |acc, num| integer::lcm(acc, num));
+    p2 = durations
+        .iter()
+        .cloned()
+        .fold(1, |acc, num| integer::lcm(acc, num));
     Ok((p1, p2))
 }
-    fn day7() -> io::Result<(usize, usize)> {
+fn day7() -> io::Result<(usize, usize)> {
     let data = fs::read_to_string("data/day7.in").unwrap();
     let (mut p1, mut p2) = (0, 0);
-    let mut cards = data.lines().flat_map(|line| line.split_whitespace()).collect::<Vec<&str>>().chunks(2).map(|ch| (ch[0], ch[1].parse::<u32>().unwrap())).collect::<Vec<(&str, u32)>>();
+    let mut cards = data
+        .lines()
+        .flat_map(|line| line.split_whitespace())
+        .collect::<Vec<&str>>()
+        .chunks(2)
+        .map(|ch| (ch[0], ch[1].parse::<u32>().unwrap()))
+        .collect::<Vec<(&str, u32)>>();
     let get_level = |card: &str| -> u32 {
         let mut freq = HashMap::new();
         card.chars().for_each(|c| *freq.entry(c).or_insert(0) += 1);
         let js = *freq.get(&'J').unwrap_or(&0);
-        let mut counts = freq.iter().filter(|&(&key,_)| key != 'J').map(|(_,&count)| count).collect::<Vec<i32>>();
+        let mut counts = freq
+            .iter()
+            .filter(|&(&key, _)| key != 'J')
+            .map(|(_, &count)| count)
+            .collect::<Vec<i32>>();
         counts.sort_unstable();
         match (counts.as_slice(), js) {
-            ([5],0) | ([4], 1) | ([3], 2) | ([2], 3) | ([1], 4) | ([], 5) => 6, // five of a kind
-            ([1, 4], 0) | ([1, 3], 1) | ([1, 2], 2)  | ([1, 1], 3)  => 5, // four of a kind
-            ([2, 3], 0) | ([2,2], 1) => 4, // FH
-            ([1, 1, 3], 0) | ([1, 1, 2], 1) | ([1, 1, 1], 2) => 3, // three of a kind
-            ([1, 2, 2], 0) => 2, // two pair
-            ([1, 1, 1, 2], 0) | ([1, 1, 1, 1], 1) => 1, // one pair
+            ([5], 0) | ([4], 1) | ([3], 2) | ([2], 3) | ([1], 4) | ([], 5) => 6, // five of a kind
+            ([1, 4], 0) | ([1, 3], 1) | ([1, 2], 2) | ([1, 1], 3) => 5,          // four of a kind
+            ([2, 3], 0) | ([2, 2], 1) => 4,                                      // FH
+            ([1, 1, 3], 0) | ([1, 1, 2], 1) | ([1, 1, 1], 2) => 3,               // three of a kind
+            ([1, 2, 2], 0) => 2,                                                 // two pair
+            ([1, 1, 1, 2], 0) | ([1, 1, 1, 1], 1) => 1,                          // one pair
             _ => 0,
         }
     };
@@ -106,16 +131,15 @@ fn day8() -> io::Result<(usize, usize)> {
         }
     };
     cards.sort_by(|a, b| {
-    let a_level = get_level(a.0);
-    let b_level = get_level(b.0);
+        let a_level = get_level(a.0);
+        let b_level = get_level(b.0);
         if a_level == b_level {
             for (a, b) in a.0.chars().zip(b.0.chars()) {
-               if card_to_num(&a) > card_to_num(&b) {
-                   return Ordering::Greater;
-               }
-               else if card_to_num(&a) < card_to_num(&b) {
-                   return Ordering::Less;
-               }
+                if card_to_num(&a) > card_to_num(&b) {
+                    return Ordering::Greater;
+                } else if card_to_num(&a) < card_to_num(&b) {
+                    return Ordering::Less;
+                }
             }
             return Ordering::Equal;
         } else {
@@ -123,7 +147,7 @@ fn day8() -> io::Result<(usize, usize)> {
         }
     });
     for (i, card) in cards.iter().enumerate() {
-       p2 += (i + 1) * card.1 as usize;
+        p2 += (i + 1) * card.1 as usize;
     }
 
     Ok((p1, p2))
@@ -132,11 +156,51 @@ fn day8() -> io::Result<(usize, usize)> {
 fn day6() -> io::Result<(u32, f64)> {
     let data = fs::read_to_string("data/day6.in").unwrap();
     let (mut p1, mut p2) = (0, 0.0);
-    let times = data.lines().next().unwrap_or("").split_whitespace().filter(|&s|  s.chars().all(|c| c.is_ascii_digit()) ).map(|s| s.parse::<f32>().unwrap()).collect::<Vec<f32>>();
-    let distances = data.lines().nth(1).unwrap_or("").split_whitespace().filter(|&s|  s.chars().all(|c| c.is_ascii_digit()) ).map(|s| s.parse::<f32>().unwrap()).collect::<Vec<f32>>();
-    p1 = times.iter().zip(distances.iter()).map(|(&t, d)| ((t + (t.powf(2.0) - 4.0 * d).sqrt()) / 2.0).ceil() - ((t - (t.powf(2.0) - 4.0 * d).sqrt()) / 2.0).floor() - 1.0).product::<f32>() as u32;
-    let newtime = data.lines().next().unwrap_or("").split_whitespace().filter(|&s|  s.chars().all(|c| c.is_ascii_digit()) ).collect::<Vec<&str>>().join("").parse::<f64>().unwrap();
-    let newdist = data.lines().nth(1).unwrap_or("").split_whitespace().filter(|&s|  s.chars().all(|c| c.is_ascii_digit()) ).collect::<Vec<&str>>().join("").parse::<f64>().unwrap();
+    let times = data
+        .lines()
+        .next()
+        .unwrap_or("")
+        .split_whitespace()
+        .filter(|&s| s.chars().all(|c| c.is_ascii_digit()))
+        .map(|s| s.parse::<f32>().unwrap())
+        .collect::<Vec<f32>>();
+    let distances = data
+        .lines()
+        .nth(1)
+        .unwrap_or("")
+        .split_whitespace()
+        .filter(|&s| s.chars().all(|c| c.is_ascii_digit()))
+        .map(|s| s.parse::<f32>().unwrap())
+        .collect::<Vec<f32>>();
+    p1 = times
+        .iter()
+        .zip(distances.iter())
+        .map(|(&t, d)| {
+            ((t + (t.powf(2.0) - 4.0 * d).sqrt()) / 2.0).ceil()
+                - ((t - (t.powf(2.0) - 4.0 * d).sqrt()) / 2.0).floor()
+                - 1.0
+        })
+        .product::<f32>() as u32;
+    let newtime = data
+        .lines()
+        .next()
+        .unwrap_or("")
+        .split_whitespace()
+        .filter(|&s| s.chars().all(|c| c.is_ascii_digit()))
+        .collect::<Vec<&str>>()
+        .join("")
+        .parse::<f64>()
+        .unwrap();
+    let newdist = data
+        .lines()
+        .nth(1)
+        .unwrap_or("")
+        .split_whitespace()
+        .filter(|&s| s.chars().all(|c| c.is_ascii_digit()))
+        .collect::<Vec<&str>>()
+        .join("")
+        .parse::<f64>()
+        .unwrap();
     let disc = (newtime.powf(2.0) - 4.0 * newdist).sqrt();
     p2 = ((newtime + disc) / 2.0).ceil() - ((newtime - disc) / 2.0).floor() - 1.0;
     Ok((p1, p2))
@@ -144,9 +208,19 @@ fn day6() -> io::Result<(u32, f64)> {
 fn day5() -> io::Result<(u64, u64)> {
     let data = fs::read_to_string("data/day5.in").unwrap();
     let (mut p1, mut p2) = (0, 0);
-    let sections = data.split("\r\n").filter(|&str| !str.is_empty()).collect::<Vec<&str>>();
-    let seeds = sections[0].split(':').nth(1).unwrap_or("").trim().split_whitespace().map(|s| s.parse::<u64>().unwrap()).collect::<Vec<u64>>();
-    let mut counter  = -1;
+    let sections = data
+        .split("\r\n")
+        .filter(|&str| !str.is_empty())
+        .collect::<Vec<&str>>();
+    let seeds = sections[0]
+        .split(':')
+        .nth(1)
+        .unwrap_or("")
+        .trim()
+        .split_whitespace()
+        .map(|s| s.parse::<u64>().unwrap())
+        .collect::<Vec<u64>>();
+    let mut counter = -1;
     let mut maps = Vec::new();
     let mut i = 1;
     while i < sections.len() {
@@ -157,28 +231,37 @@ fn day5() -> io::Result<(u64, u64)> {
         }
         let mut ranges = Vec::new();
         while i < sections.len() && !sections[i].contains("map") {
-           ranges.push(sections[i].split_whitespace().map(|s| s.parse::<u64>().unwrap()).collect::<Vec<u64>>());
+            ranges.push(
+                sections[i]
+                    .split_whitespace()
+                    .map(|s| s.parse::<u64>().unwrap())
+                    .collect::<Vec<u64>>(),
+            );
             i += 1;
         }
         maps.push(ranges);
         i += 1;
     }
 
-   let mut locations = Vec::new();
-   for seed in &seeds {
-       let mut current_num = *seed;
-       for map in &maps {
-           for range in map {
-               if current_num >= range[1] && current_num <= range[1] + range[2] {
-                   current_num = range[0] + (current_num - range[1]);
-                   break;
-               }
-           }
-       }
-       locations.push(current_num);
-   }
+    let mut locations = Vec::new();
+    for seed in &seeds {
+        let mut current_num = *seed;
+        for map in &maps {
+            for range in map {
+                if current_num >= range[1] && current_num <= range[1] + range[2] {
+                    current_num = range[0] + (current_num - range[1]);
+                    break;
+                }
+            }
+        }
+        locations.push(current_num);
+    }
     i = 0;
-    let new_seeds = seeds.chunks(2).map(|ch| ch[0]..ch[0] + ch[1]).flatten().collect::<Vec<u64>>();
+    let new_seeds = seeds
+        .chunks(2)
+        .map(|ch| ch[0]..ch[0] + ch[1])
+        .flatten()
+        .collect::<Vec<u64>>();
     // Not in mood to deal with range intersections and what not.
     let mut new_locations = Vec::new();
     for seed in new_seeds {
@@ -200,15 +283,38 @@ fn day5() -> io::Result<(u64, u64)> {
 
 fn day4() -> io::Result<(u32, u32)> {
     let data = fs::read_to_string("data/day4.in").unwrap();
-    let (mut p1, mut p2) = (0,0);
+    let (mut p1, mut p2) = (0, 0);
     let mut counts = vec![1; data.lines().count() + 1];
     for (i, line) in data.lines().enumerate() {
         let card = line.split(':').nth(1).unwrap_or("").trim();
-        let winning = card.split('|').next().unwrap_or("").trim().split_whitespace().filter(|s| !s.is_empty()).collect::<Vec<&str>>();
-        let drawn = card.split('|').nth(1).unwrap_or("").trim().split_whitespace().filter(|s| !s.is_empty()).collect::<Vec<&str>>();
-        let count = winning.iter().filter(|&c| drawn.iter().any(|&d| d == *c)).count();
-        (i+1..i+1+count).take_while(|&j| j < data.lines().count()).for_each(|j| counts[j] += counts[i]);
-        p1 += if count > 0 { u32::pow(2, (count-1) as u32) } else { 0 };
+        let winning = card
+            .split('|')
+            .next()
+            .unwrap_or("")
+            .trim()
+            .split_whitespace()
+            .filter(|s| !s.is_empty())
+            .collect::<Vec<&str>>();
+        let drawn = card
+            .split('|')
+            .nth(1)
+            .unwrap_or("")
+            .trim()
+            .split_whitespace()
+            .filter(|s| !s.is_empty())
+            .collect::<Vec<&str>>();
+        let count = winning
+            .iter()
+            .filter(|&c| drawn.iter().any(|&d| d == *c))
+            .count();
+        (i + 1..i + 1 + count)
+            .take_while(|&j| j < data.lines().count())
+            .for_each(|j| counts[j] += counts[i]);
+        p1 += if count > 0 {
+            u32::pow(2, (count - 1) as u32)
+        } else {
+            0
+        };
         p2 += counts[i];
     }
     Ok((p1, p2))
@@ -232,8 +338,8 @@ fn day3() -> io::Result<(u32, u32)> {
 
     for i in 0..lines {
         for j in 0..cols {
-            if mat[i][j] == '.' || mat[i][j].is_ascii_digit()  {
-               continue;
+            if mat[i][j] == '.' || mat[i][j].is_ascii_digit() {
+                continue;
             }
             let mut nums: Vec<u32> = Vec::new();
             for k in 0..8 {
@@ -242,7 +348,8 @@ fn day3() -> io::Result<(u32, u32)> {
                 if x < 0 || x >= lines as i32 || y < 0 || y >= cols as i32 {
                     continue;
                 }
-                if mat[x as usize][y as usize].is_ascii_digit() && used[x as usize][y as usize] == 0 {
+                if mat[x as usize][y as usize].is_ascii_digit() && used[x as usize][y as usize] == 0
+                {
                     println!("{}", mat[x as usize][y as usize]);
                     while y >= 0 && mat[x as usize][y as usize].is_ascii_digit() {
                         y -= 1;
@@ -287,13 +394,22 @@ fn day2() -> io::Result<(usize, u32)> {
         let mut maxb = 0;
         let mut is_ok = true;
         for set in sets {
-            r = 0; g = 0; b = 0;
+            r = 0;
+            g = 0;
+            b = 0;
             let extractions = set.split(",").collect::<Vec<&str>>();
             for extraction in extractions {
                 let mut j = 0;
                 let mut num = 0;
                 while extraction.trim().chars().nth(j).unwrap().is_ascii_digit() {
-                    num = num * 10 + extraction.trim().chars().nth(j).unwrap().to_digit(10).unwrap() as i32;
+                    num = num * 10
+                        + extraction
+                            .trim()
+                            .chars()
+                            .nth(j)
+                            .unwrap()
+                            .to_digit(10)
+                            .unwrap() as i32;
                     j += 1;
                 }
                 r += if extraction.contains("red") { num } else { 0 };
@@ -313,20 +429,26 @@ fn day2() -> io::Result<(usize, u32)> {
         p2 += maxr * maxg * maxb;
     }
 
-    Ok((p1,p2))
+    Ok((p1, p2))
 }
 fn day1() {
     let data = fs::read_to_string("data/day1.in").unwrap();
-    let mut lines =data.lines();
+    let mut lines = data.lines();
     let mut sum = 0;
-    let digits = vec!["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
+    let digits = vec![
+        "one", "two", "three", "four", "five", "six", "seven", "eight", "nine",
+    ];
     let parse_line = |line: &str| -> i32 {
         let parsing = |result: String, rev: bool| -> (i32, i32) {
             let mut aux = result;
             for (i, &digit) in digits.iter().enumerate() {
-                let tofind = if !rev { digit.to_string() } else { digit.chars().rev().collect::<String>() };
+                let tofind = if !rev {
+                    digit.to_string()
+                } else {
+                    digit.chars().rev().collect::<String>()
+                };
                 if let Some(pos) = aux.find(&tofind) {
-                    return (pos as i32, (i+1) as i32);
+                    return (pos as i32, (i + 1) as i32);
                 }
             }
             (999999, -1)
@@ -342,11 +464,19 @@ fn day1() {
             }
             (999999, -1)
         };
-       let first_digit = get_digit(line.to_string());
-       let last_digit = get_digit(line.to_string().chars().rev().collect::<String>());
+        let first_digit = get_digit(line.to_string());
+        let last_digit = get_digit(line.to_string().chars().rev().collect::<String>());
 
-        let first = if first_word.0 < first_digit.0 { first_word } else { first_digit };
-        let last = if last_word.0 > last_digit.0 { last_word } else { last_digit };
+        let first = if first_word.0 < first_digit.0 {
+            first_word
+        } else {
+            first_digit
+        };
+        let last = if last_word.0 > last_digit.0 {
+            last_word
+        } else {
+            last_digit
+        };
         first.1 * 10 + last.1
     };
     lines.for_each(|line| sum += parse_line(line));
